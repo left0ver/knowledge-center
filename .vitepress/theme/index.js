d535af6fabbd7709fc0c5923b9d8d8815f5ea183
left0ver <hi.leftover@qq.com>
2024-07-17 03:31:30 +0800

    修改h2的css

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import PageView from './PageView.vue'
 import 'uno.css'
+import './global.css'
 export default {
   extends: DefaultTheme,
   // 使用注入插槽的包装组件覆盖 Layout
